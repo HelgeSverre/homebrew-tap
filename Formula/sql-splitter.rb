a3d05b37e2d39637489c57de90cbcbfb003e9f7e
Helge Sverre <helge.sverre@gmail.com>
2026-08-04 23:16:06 +0200

    sql-splitter 1.19.0

--- Formula/sql-splitter.rb
+++ Formula/sql-splitter.rb
@@ -1,36 +1,36 @@
 class SqlSplitter < Formula
   desc "High-performance CLI tool for splitting large SQL dump files into individual table files"
   homepage "https://github.com/helgesverre/sql-splitter"
-  version "1.18.2"
+  version "1.19.0"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/helgesverre/sql-splitter/releases/download/v1.18.2/sql-splitter-aarch64-apple-darwin.tar.xz"
-      sha256 "926ec5d243d5c6eb26d483f6ebfcc5c61e348136b0881e6ff3a9453eb3e764c9"
+      url "https://github.com/helgesverre/sql-splitter/releases/download/v1.19.0/sql-splitter-aarch64-apple-darwin.tar.xz"
+      sha256 "e71895c4584291d5fd5b0e4207ef5518d32b7b16c8156f2d17f30fecfe10a937"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/helgesverre/sql-splitter/releases/download/v1.18.2/sql-splitter-x86_64-apple-darwin.tar.xz"
-      sha256 "230d6a0212d02055fc36e06e09b25d38146ca1c1bba06a20d62d4713ac614024"
+      url "https://github.com/helgesverre/sql-splitter/releases/download/v1.19.0/sql-splitter-x86_64-apple-darwin.tar.xz"
+      sha256 "270b7cf2fd065f99e50eb4e8cc45ab9f4c7178126f1c681d0dcd18014aee332b"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/helgesverre/sql-splitter/releases/download/v1.18.2/sql-splitter-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "b891551abe57a600d0d2452b377650709f6ef8daa96fd11cfb7e29824036c54a"
+      url "https://github.com/helgesverre/sql-splitter/releases/download/v1.19.0/sql-splitter-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "e8f6b1928a02de5d377e25d3dab3a2e781d34e83a5ca567c705e57064d4c37c1"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/helgesverre/sql-splitter/releases/download/v1.18.2/sql-splitter-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "6743807e34311f32edf40b3cfd93780815c00f598154ab5498937a7193a5d0b5"
+      url "https://github.com/helgesverre/sql-splitter/releases/download/v1.19.0/sql-splitter-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "6cd8789602b299742637ba61482758b599aa98a0d35f8d5f6ad830865fa8c784"
     end
   end
   license "MIT"
 
   BINARY_ALIASES = {
-    "aarch64-apple-darwin":      {},
+    "aarch64-apple-darwin": {},
     "aarch64-unknown-linux-gnu": {},
-    "x86_64-apple-darwin":       {},
-    "x86_64-pc-windows-gnu":     {},
-    "x86_64-unknown-linux-gnu":  {},
-  }.freeze
+    "x86_64-apple-darwin": {},
+    "x86_64-pc-windows-gnu": {},
+    "x86_64-unknown-linux-gnu": {}
+  }
 
   def target_triple
     cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
@@ -48,10 +48,18 @@ class SqlSplitter < Formula
   end
 
   def install
-    bin.install "sql-splitter" if OS.mac? && Hardware::CPU.arm?
-    bin.install "sql-splitter" if OS.mac? && Hardware::CPU.intel?
-    bin.install "sql-splitter" if OS.linux? && Hardware::CPU.arm?
-    bin.install "sql-splitter" if OS.linux? && Hardware::CPU.intel?
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "sql-splitter"
+    end
+    if OS.mac? && Hardware::CPU.intel?
+      bin.install "sql-splitter"
+    end
+    if OS.linux? && Hardware::CPU.arm?
+      bin.install "sql-splitter"
+    end
+    if OS.linux? && Hardware::CPU.intel?
+      bin.install "sql-splitter"
+    end
 
     install_binary_aliases!
 
